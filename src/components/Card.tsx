2b902c705f48e82d3b63e44b05cc5f396e5dbe0d
Juan Bautista Vega <jv604824@gmail.com>
2024-08-13 18:11:14 -0300

    Updates  Cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,12 @@
 
 export default function Card({ title, imgUrl }: { title: string, imgUrl: string }) {
     return (
-        <div className="flex flex-col  min-w-[11rem] min-h-[21rem] sm:w-[24rem] sm:h-[24rem] bg-white sm:bg-white shadow-lg hover:shadow-2xl mt-3">
+        <div className="flex flex-col min-w-[10rem] h-[18rem] items-center sm:w-[24rem] sm:h-[24rem] bg-white sm:bg-white shadow-lg hover:shadow-2xl mt-3">
             <figure>
-                <img src={imgUrl} alt="Card Preview" className=" h-[17rem] w-[21rem] sm:h-[19rem]  sm:w-[24rem]" />
+                <img src={imgUrl} alt="Card Preview" className=" h-[14rem] w-[10rem] sm:h-[19rem]  sm:w-[24rem]" />
             </figure>
-            <div className="flex flex-col p-4">
-                <div className="text-[1rem] sm:text-[1.5rem] text-center uppercase">{title}</div>
+            <div className="flex flex-col p-1">
+                <div className="text-[.9rem] sm:text-[1.5rem] text-center font-semibold uppercase">{title}</div>
             </div>
         </div>
     )
